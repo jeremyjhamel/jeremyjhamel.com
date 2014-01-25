0453036c9d530b22bf352c41bb52185ef0210aa7
Jeremy Hamel <me@jeremyjhamel.com>
2014-01-25 12:05:28 -0500

    website main stuff

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+(function($){
+	$(document).ready(function() {
+		$("#instagram").instagram({ userId: '253802488', clientId: '1c182f9c2d38407d902dbaa2a180f661', accessToken: '253802488.1c182f9.57811a498373438887dff8e86999c56d' });
+	});
+})(jQuery);
